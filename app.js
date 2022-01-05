a8366ce1d9a5c3f7c69aaf9adc5a02de5a4e00f8
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-05 19:25:29 +0300

    get request added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// importing express into the project
 const express = require('express');
+const app = express();
+
+// starting the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
+
+// data to send
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+
+// sending data with get request
+app.get('/', (req, res) => {
+  res.send(blog);
+});
