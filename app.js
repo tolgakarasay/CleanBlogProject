406f6d7e15d10d95f65f23ba7af52ba2220b94ef
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-08 01:24:59 +0300

    mongoose model and dynamic content added

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-// importing express into the project
+// IMPORTING MODULES
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const Post = require('./models/Post');
+
 const app = express();
 
+// MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//
-const ejs = require('ejs');
+// TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-// starting the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı...`);
+// DATABASE CONNECTION
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
-  res.render('index');
+// ROUTES
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -28,4 +38,16 @@ app.get('/add_post', (req, res) => {
 
 app.get('/post', (req, res) => {
   res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  // console.log(req.body);
+  res.redirect('/');
+});
+
+// STARTING THE SERVER
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
